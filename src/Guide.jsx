9e6e9e44186f37e220a7202cf86b81e55db11444
Janwrey <janvillado04@gmail.com>
2025-09-25 12:49:05 +0800

    Add files via upload

---
+++ src/Guide.jsx
@@ -0,0 +1,96 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline"> Hello world! </h1>
+
+    <div className="bg-blue-700 text-[#FFF8BC]">Jyanti Kumar</div>
+    <div>
+      <div className="border-3">Border na 3 ang kapal</div> <br />
+      <div className="border">Border</div> <br />
+      <div className="border-l-2">
+        border left <br />
+      </div>{" "}
+      <br />
+      <div className="border-x-2">
+        border left and right <br />
+      </div>{" "}
+      <br />
+      <div className="border-y-2">
+        border up and down <br />
+      </div>{" "}
+      <br />
+      <div className="border-10 border-emerald-200">
+        border na color emerald <br />
+      </div>{" "}
+      <br />
+    </div>
+    <br />
+    <div className="text-sm">Small Text</div>
+    <div className="text-md">Medium Text</div>
+    <div className="text-lg">Large Text</div>
+    <div className="text-xl">Extra Large Text</div>
+    <div className="text-2xl">2 Extra Large Text</div>
+    <div className="text-3xl">3 Extra Large Text</div>
+    <div className="text-4xl">4 Extra Large Text</div>
+    <div className="text-5xl">5 Extra Large Text</div>
+    <div className="text-6xl">6 Extra Large Text</div>
+    <div className="text-7xl">7 Extra Large Text</div>
+    <div className="text-8xl">8 Extra Large Text</div>
+    <div className="text-9xl">9 Extra Large Text</div>
+    <div className="text-[200px]">customize text</div>
+
+    <div className="font-semibold">Font Semi Bold</div>
+    <div className="font-bold">Font Bold</div>
+    <div className="italic">Font Italic</div>
+    <div className="font-extrabold">Font Extra Bold</div>
+    <div className="font-black">Font Black</div>
+    <div className="Font-mono">Font Mono</div>
+    <div className="truncate">
+      pang wrap ng text, i-adjust yung screen para makita HAHAHAHAHAHAHA
+      pandagdag lungz
+    </div>
+
+    <div>
+      <div className="border m-5">Border with margin</div>
+      <div className="border mt-15">Border with margin sa taas</div>
+      <div className="border mb-15">Border with margin sa baba</div>
+      <div className="border ml-5">Border with margin sa left</div>
+      <div className="border mr-15">Border with margin sa right</div>
+      <div className="border mx-15">Border with margin sa left and right</div>
+      <div className="border my-15">Border with margin sa top and bottom</div>
+      <div className="border p-5">Border with margin</div>
+      <div className="border pt-15">Border with margin sa taas</div> <br />
+      <div className="border pb-15">Border with margin sa baba</div> <br />
+      <div className="border pl-5">Border with margin sa left</div> <br />
+      <div className="border pr-15">Border with margin sa right</div> <br />
+      <div className="border px-15">
+        Border with margin sa left and right
+      </div>{" "}
+      <br />
+      <div className="border py-15">
+        Border with margin sa top and bottom
+      </div>{" "}
+      <br />
+    </div>
+
+    <div>
+      <div className="bg-green-200 h-96 w-96">Not customized</div>
+      <div className="bg-red-600 h-[20px] w-[20px]">customized</div>
+      <div className="h-full bg-green-600">Test</div>
+      <div className="bg-amber-400 h-lvh">Sakop buong screen</div>
+      <div className="w-1/2 md:w-full bg-blue-100">Fractional</div>
+      <div className="h-24 w-24 border overflow-hidden">
+        tanggal ng sobra
+        hahahahahahahahahahahahahahahahahhahahahahahaahahahaahahahahahahhaahah
+      </div>
+    </div>
+    <p className="text-center">Center Text</p>
+    <p className="text-end">End Text</p>
+    <div className="border-3 rounded-[50%]">Text rounded</div>
+    <div className="border-3 rounded-full">Text rounded</div>
+  </body>
+</html>;
